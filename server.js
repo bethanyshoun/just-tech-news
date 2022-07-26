f599369e75aeb1b1fd35f15281aaf22bea3143d8
bethanyshoun <bethanyshoun@gmail.com>
2022-07-26 11:12:15 -0400

    updated secret file

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: process.env.MY_SECRET,
   cookie: {},
   resave: false,
   saveUninitialized: true,
