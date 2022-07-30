dfa67c18405d1a71be13d2121f36f3c32d1ffec5
bethanyshoun <bethanyshoun@gmail.com>
2022-07-30 18:45:55 -0400

    edit and delete post pages

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const { User, Post, Comment, Vote } = require('../../models');
 
+
+
 // get all users
 router.get('/', (req, res) => {
   User.findAll({
